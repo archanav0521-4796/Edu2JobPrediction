95d09a27e46c5e598a18f0bf935316aa3fb75405
archanav0521-4796 <archanav0521@gmail.com>
2026-06-21 15:51:55 +0530

    Update API URL to Railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://edu2job-backend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
